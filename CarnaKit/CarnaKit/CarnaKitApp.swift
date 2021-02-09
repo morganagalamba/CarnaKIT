b2317a61df6ef810f8c704ff129f99a9aeea121e
Morgana <morgana.feijo@gmail.com>
2021-02-09 11:16:36 -0300

    initial project

---
+++ CarnaKit/CarnaKit/CarnaKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarnaKitApp.swift
+//  CarnaKit
+//
+//  Created by Morgana Beatriz on 09/02/21.
+//
+
+import SwiftUI
+
+@main
+struct CarnaKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
