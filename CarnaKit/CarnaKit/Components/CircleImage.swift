80a15345ab919d49e68af5acbc9b65f26dab7c01
Mirella Almeida <mirellaalmeida.1@gmail.com>
2021-02-11 20:59:45 -0300

    MapaViewadicionado

--- CarnaKit/CarnaKit/Components/CircleImage.swift
+++ CarnaKit/CarnaKit/Components/CircleImage.swift
@@ -1,16 +1,12 @@
-//
-//  CircleImage.swift
-//  CarnaKit
-//
-//  Created by Morgana Beatriz on 10/02/21.
-//
 
 import SwiftUI
 
 struct CircleImage: View {
     var body: some View {
-            Image("EuAchoEPouco")
-                
+        Image("avatar_euachoepouco")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
     }
 }
 
