dbf0d0f50681098fe44c5a7e256f93243cf03d0e
Morgana <morgana.feijo@gmail.com>
2021-02-12 01:08:39 -0300

    add 1 more card

--- CarnaKit/CarnaKit/screens/ListView.swift
+++ CarnaKit/CarnaKit/screens/ListView.swift
@@ -10,26 +10,31 @@ import SwiftUI
 struct ListView: View {
     @State private var selection = 1
     private let items: [String] = ["Favoritos","Todos"]
+    let colors = Gradient(colors: [Color("Rosa"),Color("Roxo")] )
+    
     var body: some View {
         
-        
         VStack(alignment: .leading){
+            
             Text("Blocos")
                 .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
                 .bold()
                 .padding()
-            Picker(selection: $selection, label: Text("")) {
-                           ForEach(0..<items.count, id: \.self) { index in
-                               Text(self.items[index]).tag(index)
-                           }
-                       }.pickerStyle(SegmentedPickerStyle())
+            VStack{
+                Picker(selection: $selection, label: Text("")) {
+                               ForEach(0..<items.count, id: \.self) { index in
+                                   Text(self.items[index]).tag(index)
+                               }
+                           }.pickerStyle(SegmentedPickerStyle())
+                            .padding()
+                
+            }
             
-            
-                    //Text("Value: \(selection)")
-                        .padding()
             if selection == 1 {
-                ZStack{
+                ZStack(alignment: .topTrailing){
                     Image("confetes")
+                    Image("confetes")
+                        .offset(y: 400)
                     VStack(alignment: .leading){
                         
                         ScrollView{
@@ -37,11 +42,14 @@ struct ListView: View {
                                 .font(.title2)
                                 .bold()
                             CardBlocos("Eu acho é pouco", "Second line of text in here for this card element or component","EuAchoEPouco", true)
+                            CardBlocos("Eu acho é pouco", "Second line of text in here for this card element or component","EuAchoEPouco", true)
                             
                             
                         }
                     }.padding()
-                }
+                    
+                }.background(RadialGradient(gradient: colors, center: .trailing, startRadius: 0, endRadius: 300))
+                
                 
             }
             else{
