85ab90f0498ac3c2341d1b00bc6fd77d0ea7aba0
Morgana <morgana.feijo@gmail.com>
2021-02-12 01:43:24 -0300

    add background to the list view

--- CarnaKit/CarnaKit/screens/ListView.swift
+++ CarnaKit/CarnaKit/screens/ListView.swift
@@ -8,64 +8,77 @@
 import SwiftUI
 
 struct ListView: View {
-    @State private var selection = 1
+    @State private var selection = 0
     private let items: [String] = ["Favoritos","Todos"]
     let colors = Gradient(colors: [Color("Rosa"),Color("Roxo")] )
     
     var body: some View {
-        
-        VStack(alignment: .leading){
-            
-            Text("Blocos")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                .bold()
-                .padding()
-            VStack{
-                Picker(selection: $selection, label: Text("")) {
-                               ForEach(0..<items.count, id: \.self) { index in
-                                   Text(self.items[index]).tag(index)
-                               }
-                           }.pickerStyle(SegmentedPickerStyle())
-                            .padding()
+        ZStack{
+            Rectangle()
+            Color("Roxo")
+                .ignoresSafeArea(edges: .top)
                 
-            }
-            
-            if selection == 1 {
-                ZStack(alignment: .topTrailing){
-                    Image("confetes")
-                    Image("confetes")
-                        .offset(y: 400)
-                    VStack(alignment: .leading){
-                        
-                        ScrollView{
-                            Text("Hoje")
-                                .font(.title2)
-                                .bold()
-                            CardBlocos("Eu acho é pouco", "Second line of text in here for this card element or component","EuAchoEPouco", true)
-                            CardBlocos("Eu acho é pouco", "Second line of text in here for this card element or component","EuAchoEPouco", true)
-                            
-                            
-                        }
-                    }.padding()
+            VStack(alignment: .leading){
+                
+                Text("Blocos")
+                    .font(.title)
+                    .bold()
+                    .padding()
                     
-                }.background(RadialGradient(gradient: colors, center: .trailing, startRadius: 0, endRadius: 300))
-                
-                
-            }
-            else{
-                ScrollView{
+                VStack{
+                    Picker(selection: $selection, label: Text("")) {
+                                   ForEach(0..<items.count, id: \.self) { index in
+                                       Text(self.items[index]).tag(index)
+                                   }
+                               }.pickerStyle(SegmentedPickerStyle())
+                                .padding()
                     
                 }
                 
+                if selection == 1 {
+                    ZStack(alignment: .topTrailing){
+                        Image("confetes")
+                        Image("confetes")
+                            .offset(y: 400)
+                        VStack(alignment: .leading){
+                            
+                            ScrollView{
+                                Text("Hoje")
+                                    .font(.title2)
+                                    .bold()
+                                CardBlocos("Eu acho é pouco", "Second line of text in here for this card element or component","EuAchoEPouco", true)
+                                CardBlocos("Eu acho é pouco", "Second line of text in here for this card element or component","EuAchoEPouco", true)
+                                
+                                
+                            }
+                        }.padding()
+                        
+                    }.background(RadialGradient(gradient: colors, center: .trailing, startRadius: 0, endRadius: 300))
+                    
+                    
+                }
+                else{
+                    ZStack(alignment:.topTrailing){
+                        Image("confetes")
+                            .multilineTextAlignment(.trailing)
+                        Image("confetes")
+                            .offset(y: 400)
+                        ScrollView{
+                            
+                        }
+                    }.background(RadialGradient(gradient: colors, center: .trailing, startRadius: 0, endRadius: 300))
+                    
+                }
+                
+                
             }
             
+            .background(Color("Roxo"))
+            .foregroundColor(.white)
             
         }
+        }
         
-        .background(Color("Roxo"))
-        .foregroundColor(.white)
-        
-    }
 }
 
 struct ListView_Previews: PreviewProvider {
